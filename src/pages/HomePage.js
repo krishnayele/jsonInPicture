933be01c81000d677110f09961b412c599669865
krishna yele <krishnayele@gmail.com>
2022-07-26 12:45:09 +0530

    Created Project structure

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import { Canvas } from '@react-three/fiber'
+import {Stars} from "drei"
+import React from 'react'
+
+
+function HomePage() {
+  return (
+    <Canvas>
+        <Stars/>
+    </Canvas>
+  )
+}
+
+export default HomePage
